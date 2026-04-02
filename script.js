057da10553c08a37184ab924d83ab5a39614b1bf
talin <talin.keshesh@gmail.com>
2026-04-01 20:59:31 -0700

    Part 3 and 4

--- script.js
+++ script.js
@@ -31,3 +31,37 @@ document.getElementById("conversion-result-3").textContent =
 
 document.getElementById("conversion-result-4").textContent =
     `Original value: "${valueFour}" → Converted: ${convertedFour} → isNaN: ${Number.isNaN(convertedFour)} → isInteger: ${Number.isInteger(convertedFour)}`;
+
+
+
+
+    //Part 3
+const itemPrice = 24.99;
+const taxRate = 0.10;
+const shippingCost = 5.50;
+
+const subtotal = itemPrice + shippingCost;
+const taxAmount = itemPrice * taxRate;
+const totalCost = subtotal + taxAmount;
+const roundedTotal = totalCost.toFixed(2);
+
+document.getElementById("math-result-1").textContent =
+    `Item price: $${itemPrice}`;
+
+document.getElementById("math-result-2").textContent =
+    `Shipping cost: $${shippingCost} / Subtotal: $${subtotal.toFixed(2)}`;
+
+document.getElementById("math-result-3").textContent =
+    `Tax amount: $${taxAmount.toFixed(2)}`;
+
+document.getElementById("math-result-4").textContent =
+    `Total cost: $${roundedTotal}`;
+
+
+
+//Part 4
+if (totalCost >= 30) {
+    document.getElementById("math-message").textContent = "The total cost is $30.00 or more.";
+} else {
+    document.getElementById("math-message").textContent = "The total cost is less than $30.00.";
+}
